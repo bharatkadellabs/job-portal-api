f3cc8df3e8cf080518e7a0190eeb50d7883ff80a
bharatkadellabs <106664708+bharatkadellabs@users.noreply.github.com>
2022-11-03 23:41:38 +0530

    heroku ada

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 require('dotenv').config()
-import express, { json, urlencoded } from 'express';
-import { connect } from 'mongoose';
-import cors from 'cors';
-import indexRouter from './router/index';
+const express = require('express')
+const mongoose = require('mongoose');
+const cors = require('cors');
+const indexRouter = require('./router/index')
 const app = express()
 const url = process.env.DB_URL
-connect(url).then((res) => console.log("connect Data base"));
+mongoose.connect(url).then((res) => console.log("connect Data base"));
 const port = 8000;
-import path from 'path';
+const path = require('path')
 
-app.use(json());
-app.use(urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // app.use(express.static(path.join(__dirname, "public")));
 //To allow cross-origin requests
 app.use(cors());
