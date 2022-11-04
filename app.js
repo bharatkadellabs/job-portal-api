9986c400e65b174307aaa6cd71c22dfcc0b6c932
bharatkadellabs <106664708+bharatkadellabs@users.noreply.github.com>
2022-11-04 18:52:50 +0530

    d

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = 8000;
 const path = require('path')
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 //To allow cross-origin requests
 app.use(cors());
 app.use("/", indexRouter);
