eba809ee55d97354ede43bede8f6b14d003fc6dd
bharatkadellabs <106664708+bharatkadellabs@users.noreply.github.com>
2022-11-06 12:36:21 +0530

    aasaqq

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const indexRouter = require('./router/index')
 const app = express()
 const url = process.env.DB_URL
 mongoose.connect(url).then((res) => console.log("connect Data base"));
-const port = 8000;
+const PORT = process.env.PORT || 8081;
 const path = require('path')
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,4 +15,4 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use("/", indexRouter);
 
-app.listen(port, () => console.log(`running on ports ${port}`))
+app.listen(PORT, () => console.log(`running on ports ${PORT}`))
